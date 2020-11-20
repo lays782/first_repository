ef6ef7677764fb15b0802a714b053b2ba2258a52
lays782 <56540694+lays782@users.noreply.github.com>
2020-11-20 08:34:29 +0100

    Added something

---
+++ program.py
@@ -0,0 +1,12 @@
+
+file = open("C:\\Users\\jirik\\Documents\\Moje dokumenty\\Cancelled flights\\Email_templates\\affected_passengers.txt", "r")
+
+text = file.readlines()
+
+
+for i in text:
+    print(i)
+    
+
+
+file.close()
